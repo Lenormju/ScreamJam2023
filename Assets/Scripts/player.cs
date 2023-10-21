048444f121d33af67b096a82f2e7c3708a1be61e
Your Name <you@example.com>
2023-10-21 11:45:37 +0200

    Added movement for player

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class player : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _rotSpeed = 10f;
 
     Rigidbody2D rb;
-    Vector3 normalizedSpeedVector;
+    float normalizedSpeed;
 
     void Start()
     {
@@ -23,7 +24,10 @@ public class player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical"); 
-        normalizedSpeedVector = new Vector3(horizontalInput, verticalInput, 0).normalized * _speed;
-        rb.MovePosition(gameObject.transform.position + normalizedSpeedVector * Time.fixedDeltaTime);
+        normalizedSpeed = verticalInput * _speed;
+
+        Debug.Log(horizontalInput + " " + verticalInput);
+        rb.MovePosition(gameObject.transform.position + gameObject.transform.up * normalizedSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation - horizontalInput * _rotSpeed * Time.fixedDeltaTime);
     }
 }
