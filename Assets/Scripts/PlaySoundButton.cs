56925c88d21aeb78b9ea2ec123a3c8dbc120d231
Lou BG <lou.bege@kaizen-solutions.net>
2023-10-21 18:18:25 +0200

    add sound on settings

--- Assets/Scripts/PlaySoundButton.cs
+++ Assets/Scripts/PlaySoundButton.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class PlaySoundButton : MonoBehaviour, IPointerEnterHandler
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public AudioSource audioData;
+    public bool WantPause = false;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        audioData.Play(0);
+        audioData.Play();
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if (WantPause)
+        {
+            audioData.Stop();
+        }
     }
 }
