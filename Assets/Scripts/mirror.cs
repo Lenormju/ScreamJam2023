63524d86087793f911a270f0063f13162ad27cff
Julien PINJON <julien.pinjon@kaizen-solutions.net>
2023-10-21 09:58:22 +0200

    Mirroir mon beau mirroir

--- Assets/Scripts/mirror.cs
+++ Assets/Scripts/mirror.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class mirror : MonoBehaviour
 {
-
-    public SpriteRenderer sp_fantome;
+    public GameObject player;
+    public GameObject fantome;
+    SpriteRenderer _spFantome;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     _spFantome = fantome.GetComponent<SpriteRenderer>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fantome.transform.position = new Vector3(player.transform.position.x, Mathf.Abs(player.transform.position.y), 0);
+        float scaleRatio = 3f / (1 + 0.2f * Mathf.Abs(player.transform.position.y));
+        fantome.transform.localScale = new Vector3(scaleRatio, scaleRatio, 0); 
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player")){
             Debug.Log("Toc toc ! entrez !");
-            sp_fantome.enabled = true;
+            _spFantome.enabled = true;
         }
         
     }
@@ -32,7 +35,7 @@ public class mirror : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player")){
             Debug.Log("Dégage");
-            sp_fantome.enabled = false;
+            _spFantome.enabled = false;
         }
         
     }
